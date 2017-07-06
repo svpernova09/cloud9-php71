b043e82973122a4273fc160e1d25129c402a2904
Joe Ferguson <joe@joeferguson.me>
2017-07-06 15:12:59 -0500

    ✨ 🎉 initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Install PHP 7.1 on a Cloud 9 Workspace
+
+### Usage
+
+```
+sh install.sh
+```
+
+Verify installation by viewing info.php in your workspace's url
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update -y
+
+sudo apt-get install php7.1-curl php7.1-cli php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-json php7.1-mysql php7.1-opcache php7.1-bcmath php7.1-mbstring php7.1-soap php7.1-xml php7.1-zip -y
+
+sudo mv /etc/apache2/envvars /etc/apache2/envvars.bak
+sudo apt-get remove libapache2-mod-php5 -y
+sudo apt-get install libapache2-mod-php7.1 -y
+sudo cp /etc/apache2/envvars.bak /etc/apache2/envvars
+
+sudo a2dismod php5
+sudo a2enmod php7.1
+
+sudo service apache2
